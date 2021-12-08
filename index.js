8a7f3e1a6453a10d539b34e3bd87724a6c027358
Kalixer <al.caceres.ag72@gmail.com>
2021-12-08 17:16:41 -0300

    Interacción directamente en el DOM, no es necesario abrir la consola

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function diametroCirculo(radio) {
 
 // Circunferencia
 
-function perimetroCirculo(radio) {
+function circunferenciaCirculo(radio) {
     const diametro = diametroCirculo(radio);
     return diametro * Math.PI;
 }
@@ -54,3 +54,52 @@ function areaCirculo(radio) {
 }
 
 console.groupEnd();
+
+
+// Trigger HTML
+
+// Cuadrado
+
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const perimetro = perimetroCuadrado(input.value);
+    alert("El perímetro es de " + perimetro + "cm")
+}
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById("InputCuadrado");
+    const area = areaCuadrado(input.value);
+    alert("El área es de " + area + "cm")
+}
+
+
+// Triángulo
+
+function calcularPerimetroTriangulo() {
+    const input1 = document.getElementById("InputTriangulo1");
+    const input2 = document.getElementById("InputTriangulo2");
+    const input3 = document.getElementById("InputTriangulo3");
+    const perimetro = perimetroCuadrado(input1.value, input2.value, input3.value);
+    alert("El perímetro es de " + perimetro + "cm")
+}
+
+function calcularAreaTriangulo() {
+    const input1 = document.getElementById("InputBase");
+    const input2 = document.getElementById("InputAltura");
+    const area = areaTriangulo(input1.value, input2.value);
+    alert("El área es de " + area + "cm")
+}
+
+// Círculo
+
+function calcularCircunferenciaCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const circunferencia = circunferenciaCirculo(input.value)
+    alert("El perímetro es de " + circunferencia + "cm")
+}
+
+function calcularAreaCirculo() {
+    const input = document.getElementById("InputCirculo");
+    const area = areaCirculo(input.value);
+    alert("El área es de " + area + "cm")
+}
